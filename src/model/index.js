b7c20efa23e738fcbe0696774f57ec288fffd9fa
Jasmineb2011 <Teddybear13.TB@gmail.com>
2019-06-10 12:45:59 +0300

    Added an exampe of the db

---
+++ src/model/index.js
@@ -0,0 +1,5 @@
+module.exports = [
+    { student_id: 6544489, full_name: 'Jazz Badrih', password: 'jb123456', email: 'Jasmine@gmail.com' },
+    { student_id: 2455681, full_name: 'Enass Kmal', password: 'ek112233', email: 'Enass@gmail.com' },
+    { student_id: 2457935, full_name: 'Hoda Mansour', password: 'hm54321', email: 'Hoda@gmail.com' }
+]
