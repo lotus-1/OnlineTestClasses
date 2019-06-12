c2e72a65cbf23a8e57dd26b2623dbcc28b116c24
hodamansour <hodamansour164@gmail.com>
2019-06-12 11:50:23 +0300

    coding the hashing files

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ const { celebrate } = require('celebrate');
 const  register = require('../views/register');
 const validate = require('../helpers/validate');
 const { loginValidation, signupValidation } = require('../helpers/validation');
+const hashingPassword = require('../helpers/hashing');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -16,9 +17,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/register', validate(signupValidation), (req, res) => {
-  res.render('register'), {
-    title: 'register'
-  };
+  res.render(path.join(__dirname, "..", "views", "register.hbs" ));
+
 });
 
 router.post('/register', (req, res) => {
