0039dddbc941c3059207c63abb62ee33977c4d21
enasskmal <enasskmal@gmail.com>
2019-06-13 11:28:13 +0300

    adding the endpoint student

--- tests/db_tests.js
+++ tests/db_tests.js
@@ -9,29 +9,29 @@ tape("tape is working", t => {
   t.equals(1, 1, "one equals one");
   t.end();
 });
-//
-// tape("getData", t => {
-// buildDatabase(function(err, res) {
-//     t.error(err, "No Error");
-//     //Assert that db_build finished successfully with no errors
-//
-//     let expected = [
-//       {
-//         student_id: 20076678,
-//         full_name: 'Enass Kmal',
-//         email: 'enass@gmail.com',
-//         password: '123456'
-//       }
-//     ];
-//
-//     getData((err, result) => {
-//       console.log("this inside the 2 test");
-//       if (err) console.log(err);
-//       t.deepEqual(result, expected, "Returns expected data");
-//       t.end();
-//     });
-//   });
-// });
+
+tape("getData", t => {
+buildDatabase(function(err, res) {
+    t.error(err, "No Error");
+    //Assert that db_build finished successfully with no errors
+
+    let expected = [
+      {
+        student_id: 20076678,
+        full_name: 'Enass Kmal',
+        email: 'enass@gmail.com',
+        password: '123456'
+      }
+    ];
+
+    getData((err, result) => {
+      console.log("this inside the 2 test");
+      if (err) console.log(err);
+      t.deepEqual(result, expected, "Returns expected data");
+      t.end();
+    });
+  });
+});
 
 
 
